24b91a38ba6704b9ddf5c7d842dba2d82da53234
Gabriel <gabrieljaimecanooquendo@gmail.com>
2022-11-26 13:12:38 -0500

    Se sube el taller final

---
+++ src/app/services/habitaciones.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HabitacionesService {
+
+  constructor(public peticion:HttpClient) { }
+
+  consultarHabitaciones():Observable<any>{
+    let uri = "https://serve20222.herokuapp.com/hotelesnick/habitaciones";
+
+    return this.peticion.get(uri)
+  }
+  consultarHabitacionPorId():Observable<any>{
+    
+    let uri = "https://serve20222.herokuapp.com/hotelesnick/habitaciones";
+
+    let id = "id";
+
+    return this.peticion.get(uri+id)
+  }
+}
